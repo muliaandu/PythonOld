08045020d333a1ded6c4e8766020355128d8725b
muliaandu <robins.andu@gmail.com>
2023-10-06 01:25:13 +0700

    Robin

---
+++ Work Project/WA OTP/WA_Check_Account.py
@@ -0,0 +1,32 @@
+class Whatsapp_Check_Account():
+        def post(self):
+
+        if not current_user.akerun_user_id:
+            user_auth = default
+
+            r = requests.post(
+                akerun_url,
+                headers,
+                akerun_user_id,
+                email,
+                user_auth
+            )
+            return r(), 200
+        else:
+            pass
+
+
+class Whatsapp_():
+    def post(self):
+
+
+        r = request.post(
+                    headers, 
+                    akerun_user_id,
+                    schedule,
+                    user_id.booking[start_datetime],
+                    user_id.booking[end_datetime],
+                    role
+                    )
+        
+        return r(), 200
