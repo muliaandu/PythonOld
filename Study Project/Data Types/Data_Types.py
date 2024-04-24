ea0be2308abb18d548e32b70c1f35bebdd38acdc
muliaandu <robins.andu@gmail.com>
2024-04-24 23:48:05 +0700

    laptop

--- Study Project/Data Types/Data_Types.py
+++
@@ -1,6 +0,0 @@
-# 1. Ask for two digit number.
-two_digit_number = str(input('Type a two digit number (10 - 99) : '))
-# 2. Print result from first digit plus second digit.
-first_digit = two_digit_number[0]
-second_digit = two_digit_number[1]
-print("{} + {} = {}".format(first_digit, second_digit, int(first_digit) + int(second_digit)))
--- Study Project/ISS API/main.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-response = requests.get(url = "http://api.open-notify.org/iss-now.json")
-print(response)
-response.raise_for_status()
-
-data = response.json()
-print(data)
